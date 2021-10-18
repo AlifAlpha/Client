9edde74f5fdb9128f2161f74ad888413cdf86fa2
Amine <chegdali.amine@gmail.com>
2021-10-18 20:08:32 +0100

    ok

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -2,11 +2,18 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Admin from "./Admin/Admin";
 import Home from "./Home";
+import LeaveRequestForm from "./LeaveRequestForm";
 
 const MainRouter = () => {
   return (
     <Switch>
       <Route exact path='/' component={Home}></Route>
+      <Route
+        exact
+        path='/LeaveRequestForm'
+        component={LeaveRequestForm}
+      ></Route>
+
       <Route exact path='/dashboard' component={Admin}></Route>
     </Switch>
   );
