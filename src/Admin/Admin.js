bad1dd1da692bebb834dbc18998773a8998c4d31
BAKCHA Sohaib Arbi <51241996+SohaibArbiBakcha@users.noreply.github.com>
2021-11-28 17:59:22 +0100

    change link

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -44,7 +44,7 @@ const Dashboard = () => {
   return (
     <Admin
       authProvider={auth}
-      dataProvider={restProvider("https://icesco.herokuapp.com/")}
+      dataProvider={restProvider("https://icesco.herokuapp.com")}
     >
       <Resource
         name='travelinfo'
